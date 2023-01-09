bb8a311b60fd74b2b94bf900145495e8c86fd1fd
ShorenaK <shorenaanzhilov@gmail.com>
2023-01-09 14:26:32 -0500

    index css

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const myEmojis = ["👨‍💻", "⛷", "🍲"]
+const myEmojis = ["📲", "⛷", "🍲"]
 
+for (let i = 0; i < myEmojis.length; i ++){
+ console.log(myEmojis[i])
+}
+
+const div = document.getElementById("emojiContainer")
+console.log(div)
