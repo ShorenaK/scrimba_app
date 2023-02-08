f77b37aa1d551dff40e3498fb2c17fcb3de8ad84
ShorenaK <shorenaanzhilov@gmail.com>
2023-02-07 20:44:51 -0500

    buttons added

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const myEmojis = ["💻", "🤩", "🫂"]
 
 const emojiContainer = document.getElementById("emojiContainer")
+const button = document.getElementById("push-btn")
+const unshifButton = document.getElementById("unshift-btn")
+const input = document.querySelector("#emoji-input")
 
- let button = document.getElementById("push-btn")
- 
+
+ function render(){
+     emojiContainer.innerHTML = ""
+    for (let i = 0; i < myEmojis.length; i ++){
+        const emojis =  document.createElement("span")
+        emojis.textContent = myEmojis[i]
+        emojiContainer.appendChild(emojis)
+     }
+   }
+
  button.addEventListener('click', (e)=>{
-    const input = document.querySelector("#emoji-input")
     if(input.value){
         myEmojis.push(input.value)
         input.value = ""
-        emojiContainer.innerHTML = ""
+     
         render()
   }   
  })
-function render(){
- for (let i = 0; i < myEmojis.length; i ++){
-    const emojis =  document.createElement("span")
-     emojis.textContent = myEmojis[i]
-     emojiContainer.appendChild(emojis)
 
-  }
-}
+ unshifButton.addEventListener('click', function(e){
+    if(input.value){
+        myEmojis.unshift(input.value)
+        input.value = ""
+        render()
+  }   
+ })
+
