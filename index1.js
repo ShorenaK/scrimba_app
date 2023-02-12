66ba144eacf1474e6e1d302c9c7031697057dba1
ShorenaK <shorenaanzhilov@gmail.com>
2023-02-11 20:40:43 -0500

    inline styleing in JS file

--- index1.js
+++ index1.js
@@ -8,6 +8,16 @@ revealBtn.addEventListener('click', function(){
     revealBtn.style.display ='none'
     question.style.background = '#68e1fd'
     question.style.color = '#1434A4'
-    
-    
+   
+})
+
+const modal = document.getElementById('modal')
+const modalCloseBtn = document.getElementById('modal-close-btn')
+ 
+setTimeout(function(){
+    modal.style.display = 'inline'
+}, 1500)
+
+modalCloseBtn.addEventListener('click', function(){
+    modal.style.display = 'none'
 })
