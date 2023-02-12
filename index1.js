a96cb6683a6873c7fc0e8078703c721f151d2f63
ShorenaK <shorenaanzhilov@gmail.com>
2023-02-11 20:27:52 -0500

    .style.color in js

--- index1.js
+++ index1.js
@@ -1,3 +1,13 @@
 setTimeout(() => {
     console.log("Modal Opened")
 }, 1500);
+
+
+revealBtn.addEventListener('click', function(){
+    answer.style.display = 'block'
+    revealBtn.style.display ='none'
+    question.style.background = '#68e1fd'
+    question.style.color = '#1434A4'
+    
+    
+})
