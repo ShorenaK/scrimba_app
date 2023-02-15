28363c55ac0a4fdaed919ed7ab49a2d30ccb41ad
ShorenaK <shorenaanzhilov@gmail.com>
2023-02-15 14:25:41 -0500

    for of loop implemented

--- index3.js
+++ index3.js
@@ -169,41 +169,31 @@ const catsData = [
     },
 ]
 
+// function getEmotionsArray(cats){
+//     const emotionArray = []
+//     for(let i = 0; i < cats.length; i ++){
+//         for(let j = 0; j < cats[i].emotionTags.length; j ++){
+//             emotionArray.push(cats[i].emotionTags[j])
+//         }
+//     }
+//     console.log(emotionArray)
+// }
+
+// getEmotionsArray(catsData)
 function getEmotionsArray(cats){
-    const emotionArray = []
-    for(let i = 0; i < cats.length; i ++){
-        for(let j = 0; j < cats[i].emotionTags.length; j ++){
-            emotionArray.push(cats[i].emotionTags[j])
+    for (let cat of cats){
+        for (emotion of cat.emotionTags){
+
         }
-    }
-    console.log(emotionArray)
+        console.log(emotion)
+    }   
 }
 
 getEmotionsArray(catsData)
-const characters = [
-    {
-        title: 'Ninja',
-        emoji: '🥷',
-        powers: ['agility', 'stealth', 'aggression'],
-    },
-    {
-        title: 'Sorcerer',
-        emoji: '🧙',
-        powers: ['magic', 'invisibility', 'necromancy'],
-    },
-    {
-        title: 'Ogre',
-        emoji: '👹',
-        powers: ['power', 'stamina', 'shapeshifting'],
-    },
-    {
-        title: 'Unicorn',
-        emoji: '🦄',
-        powers: [ 'flight', 'power', 'purity'],
-    }
-]
 
-for ( let character of characters){
-    console.log(character)
 
-}
+// for ( let character of characters){
+//     for (let power of character.powers){   
+//         console.log(power)
+//     }
+// }
