6434ad3c6bec30a7f6664e85f3b9d3d746843f93
ShorenaK <shorenaanzhilov@gmail.com>
2023-02-17 16:11:17 -0500

    adding include()to function

--- index3.js
+++ index3.js
@@ -16,8 +16,10 @@ function getEmotionsArray(cats){
     const emotionsArray = []
     for (let cat of cats){
         for (let emotion of cat.emotionTags){
+            if(!emotionsArray.includes(emotion)){   
             emotionsArray.push(emotion)
         }
+      }
     }  
     return emotionsArray
 }
